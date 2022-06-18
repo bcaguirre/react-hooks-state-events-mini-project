eea47f7ec7c78868ba73c9b52a91040fe424fce1
bernadette <aguirre.bernadette@outlook.com>
2022-06-18 14:47:59 -0600

    Completed Assignment

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,23 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+//TaskList
+//First, we'll want to display all the tasks in our app. 
+//Pass down the task data from App to TaskList, and display each task using the Task component. 
+//Make sure to use a key prop!
+
+
+function TaskList({ TASKS }) {
+  
   return (
     <div className="tasks">
+      <Task  />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
       {/* display a list of tasks using Task component */}
     </div>
   );
